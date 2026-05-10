GideonZ/1541ultimate

Reduced memory use of hex viewer and using Editor inheritance hierarchy

software/userinterface/user_file_interaction.cc
@@ -13,9 +13,9 @@
 
 #include "home_directory.h"
 #include "subsys.h"
+#include "editor.h"
 
 #define MAX_FILE_SIZE_TO_VIEW 262144
-#define MAX_FILE_SIZE_TO_HEX_VIEW 26 * 1024 // TODO Investigate how this limit can be increased
 
 // member
 int UserFileInteraction::fetch_context_items(BrowsableDirEntry *br, IndexedList<Action *> &list)
@@ -35,12 +35,8 @@ int UserFileInteraction::fetch_context_items(BrowsableDirEntry *br, IndexedList<
     }
     if ((info->size <= MAX_FILE_SIZE_TO_VIEW) && (!(info->attrib & AM_DIR))) {
         list.append(new Action("View", UserFileInteraction::S_view, 0));
-        if (info->size <= MAX_FILE_SIZE_TO_HEX_VIEW) {
-            printf("Hex view: file size %d <= %d\n", info->size, MAX_FILE_SIZE_TO_HEX_VIEW);
-            list.append(new Action("Hex View", UserFileInteraction::S_hex_view, 0));
-            count++;
-        }
-        count++;
+        list.append(new Action("Hex View", UserFileInteraction::S_hex_view, 0));
+        count += 2;
     }
     if (info->is_writable()) {
         list.append(new Action("Rename", UserFileInteraction::S_rename, 0));
@@ -139,7 +135,7 @@ int UserFileInteraction::S_delete(SubsysCommand *cmd)
     return 0;
 }
 
-int _view(SubsysCommand *cmd, bool hex)
+int _view(SubsysCommand *cmd, EditorType editor_type)
 {
     FileManager *fm = FileManager::getFileManager();
     File *f = 0;
@@ -149,12 +145,15 @@ int _view(SubsysCommand *cmd, bool hex)
         uint32_t size = f->get_size();
         char *text_buf = new char[size + 1];
         FRESULT fres = f->read(text_buf, size, &transferred);
-        printf("Res = %d. Read text buffer: %d bytes\n", fres, transferred);
+        printf("Res = %d. Read text buffer: %d bytes. File size: %d bytes\n", fres, transferred, size);
         text_buf[transferred] = 0;
-        if (hex) {
-            cmd->user_interface->run_hex_editor(text_buf, transferred);
-        } else {
-            cmd->user_interface->run_editor(text_buf, transferred);
+        switch (editor_type) {
+            case HEX_EDITOR:
+                cmd->user_interface->run_hex_editor(text_buf, transferred);
+                break;
+            default:
+                cmd->user_interface->run_editor(text_buf, transferred);
+                break;
         }
         delete text_buf;
     }
@@ -163,12 +162,12 @@ int _view(SubsysCommand *cmd, bool hex)
 
 int UserFileInteraction::S_view(SubsysCommand *cmd)
 {
-    return _view(cmd, false);
+    return _view(cmd, TEXT_EDITOR);
 }
 
 int UserFileInteraction::S_hex_view(SubsysCommand *cmd)
 {
-    return _view(cmd, true);
+    return _view(cmd, HEX_EDITOR);
 }
 
 int UserFileInteraction::S_createDir(SubsysCommand *cmd)

jusii/prkl_ultimate

(cherry picked from commit 600d943979e6054db9b28775e65eda9142399292)

software/userinterface/user_file_interaction.cc
@@ -13,9 +13,9 @@
 
 #include "home_directory.h"
 #include "subsys.h"
+#include "editor.h"
 
 #define MAX_FILE_SIZE_TO_VIEW 262144
-#define MAX_FILE_SIZE_TO_HEX_VIEW 26 * 1024 // TODO Investigate how this limit can be increased
 
 // member
 int UserFileInteraction::fetch_context_items(BrowsableDirEntry *br, IndexedList<Action *> &list)
@@ -35,12 +35,8 @@ int UserFileInteraction::fetch_context_items(BrowsableDirEntry *br, IndexedList<
     }
     if ((info->size <= MAX_FILE_SIZE_TO_VIEW) && (!(info->attrib & (AM_DIR | AM_VOL)))) {
         list.append(new Action("View", UserFileInteraction::S_view, 0));
-        if (info->size <= MAX_FILE_SIZE_TO_HEX_VIEW) {
-            printf("Hex view: file size %d <= %d\n", info->size, MAX_FILE_SIZE_TO_HEX_VIEW);
-            list.append(new Action("Hex View", UserFileInteraction::S_hex_view, 0));
-            count++;
-        }
-        count++;
+        list.append(new Action("Hex View", UserFileInteraction::S_hex_view, 0));
+        count += 2;
     }
     if (info->is_writable() && !(info->attrib & AM_VOL)) {
         list.append(new Action("Rename", UserFileInteraction::S_rename, 0));
@@ -140,7 +136,7 @@ SubsysResultCode_e UserFileInteraction::S_delete(SubsysCommand *cmd)
     return SSRET_OK;
 }
 
-static SubsysResultCode_e view_file(SubsysCommand *cmd, bool hex)
+static SubsysResultCode_e view_file(SubsysCommand *cmd, EditorType editor_type)
 {
     FileManager *fm = FileManager::getFileManager();
     File *f = 0;
@@ -150,12 +146,15 @@ static SubsysResultCode_e view_file(SubsysCommand *cmd, bool hex)
         uint32_t size = f->get_size();
         char *text_buf = new char[size + 1];
         FRESULT fres = f->read(text_buf, size, &transferred);
-        printf("Res = %d. Read text buffer: %d bytes\n", fres, transferred);
+        printf("Res = %d. Read text buffer: %d bytes. File size: %d bytes\n", fres, transferred, size);
         text_buf[transferred] = 0;
-        if (hex) {
-            cmd->user_interface->run_hex_editor(text_buf, transferred);
-        } else {
-            cmd->user_interface->run_editor(text_buf, transferred);
+        switch (editor_type) {
+            case HEX_EDITOR:
+                cmd->user_interface->run_hex_editor(text_buf, transferred);
+                break;
+            default:
+                cmd->user_interface->run_editor(text_buf, transferred);
+                break;
         }
         delete text_buf;
     }
@@ -164,12 +163,12 @@ static SubsysResultCode_e view_file(SubsysCommand *cmd, bool hex)
 
 SubsysResultCode_e UserFileInteraction::S_view(SubsysCommand *cmd)
 {
-    return view_file(cmd, false);
+    return view_file(cmd, TEXT_EDITOR);
 }
 
 SubsysResultCode_e UserFileInteraction::S_hex_view(SubsysCommand *cmd)
 {
-    return view_file(cmd, true);
+    return view_file(cmd, HEX_EDITOR);
 }
 
 SubsysResultCode_e UserFileInteraction::S_createDir(SubsysCommand *cmd)